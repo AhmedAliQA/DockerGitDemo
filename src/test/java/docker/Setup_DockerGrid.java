28a41396f27355c8f6ee1ddd4c00273c8acb8491
AhmedAliQA <aali@eastnets.com>
2024-04-30 17:20:59 +0300

    remove yaml file

--- src/test/java/docker/Setup_DockerGrid.java
+++ src/test/java/docker/Setup_DockerGrid.java
@@ -11,22 +11,22 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 public class Setup_DockerGrid {
-	@SuppressWarnings({ })
-	@BeforeTest
-	void startDocker() throws IOException, InterruptedException
-	{
-		Runtime.getRuntime().exec("cmd /c start start_dockergrid.bat");
-		Thread.sleep(15000);
-		System.out.println(" ---------Docker Started----------");
-
-	}
-	@SuppressWarnings("deprecation")
-	@AfterTest
-	void stopDocker() throws IOException, InterruptedException
-	{
-		Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
-		Thread.sleep(15000);
-		System.out.println(" ---------Docker Stoped----------");
-	}
+	/*
+	 * @SuppressWarnings({ })
+	 * 
+	 * @BeforeTest void startDocker() throws IOException, InterruptedException {
+	 * Runtime.getRuntime().exec("cmd /c start start_dockergrid.bat");
+	 * Thread.sleep(15000);
+	 * System.out.println(" ---------Docker Started----------");
+	 * 
+	 * }
+	 * 
+	 * @SuppressWarnings("deprecation")
+	 * 
+	 * @AfterTest void stopDocker() throws IOException, InterruptedException {
+	 * Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
+	 * Thread.sleep(15000); System.out.println(" ---------Docker Stoped----------");
+	 * }
+	 */
 
 }
